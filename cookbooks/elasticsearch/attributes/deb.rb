905752c10738ea12f301b727fe9cc11fd67b631f
Brenno Costa <brennolncosta@gmail.com>
2015-07-02 21:24:25 +0000

    :up: Update cookbooks

--- cookbooks/elasticsearch/attributes/deb.rb
+++ cookbooks/elasticsearch/attributes/deb.rb
@@ -1,2 +1,2 @@
-default.elasticsearch[:deb_url] = 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.5.2.deb'
-default.elasticsearch[:deb_sha] = '9ed048804e3e585610fd18187dbd2ef6a70f085b4b68a18c4746b245ff673937'
+default.elasticsearch[:deb_url] = 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.6.0.deb'
+default.elasticsearch[:deb_sha] = 'ac3b84b355234e491fbaaa9e27bcd4be1304fcfd36bb8a1c7ce9920f77c727ad'
